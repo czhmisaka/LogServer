b681bc7143a7c6129d6a475854d2301d1c1fca8b
陈之罕 <wb-czh931449@cainiao.com>
2021-06-29 19:09:26 +0800

    czh
    Signed-off-by: 陈之罕 <wb-czh931449@cainiao.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r",encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="downtool", 
+    name="LogServer", 
     version="0.0.1",
     author="chen zhihan",
     license='MIT Licence',
