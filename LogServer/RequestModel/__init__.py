bf23e8d4996d412ad5a92c8d73bce5c273dc01ac
czhmisaka <czh2233445566@163.com>
2022-02-13 16:11:45 +0800

    czh

--- LogServer/RequestModel/__init__.py
+++ LogServer/RequestModel/__init__.py
@@ -1,9 +1,9 @@
 '''
 Date: 2022-02-13 15:09:09
 LastEditors: CZH
-LastEditTime: 2022-02-13 15:09:09
+LastEditTime: 2022-02-13 15:10:12
 FilePath: /LogServer/LogServer/RequestModel/__init__.py
 '''
 
 
-from IOClass import *
+from .IOClass import *
