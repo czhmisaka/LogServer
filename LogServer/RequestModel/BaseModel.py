61ad7ff8adf705ae7be71e10b6f9b5501b58419c
czhmisaka <czh2233445566@163.com>
2022-02-14 08:39:42 +0800

    a

---
+++ LogServer/RequestModel/BaseModel.py
@@ -0,0 +1,39 @@
+'''
+Date: 2022-02-13 16:12:11
+LastEditors: CZH
+LastEditTime: 2022-02-13 17:35:27
+FilePath: /LogServer/LogServer/RequestModel/BaseModel.py
+'''
+from asyncio.constants import SENDFILE_FALLBACK_READBUFFER_SIZE
+from enum import Enum
+from pydantic import BaseModel
+from typing import Optional
+
+
+class RequestType(Enum):
+    SUCCESS = "Success"
+    FAILED = "Fail"
+    ERROR= "Error"
+    WARNING = "Warning"
+    
+
+class BaseRequest(BaseModel):
+    data: Optional[dict] = {}
+    status: Optional[str]
+    
+
+
+
+class Request(BaseModel):
+    def success(self,data):
+        return BaseRequest(data = data,status = RequestType.SUCCESS)
+
+    def fail(self,data):
+        return BaseRequest(data = data,status = RequestType.FAILED)
+
+    def error(self,data):
+        return BaseRequest(data = data,status = RequestType.ERROR)
+
+
+    def warn(self,data):
+        return BaseRequest(data = data,status = RequestType.WARNING)
